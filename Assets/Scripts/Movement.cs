e5ca3793a749d11df7bd081bda251b73f9a260a5
Kaan Ozkilic <kaanic1337@gmail.com>
2024-12-09 23:10:39 +0200

    rocket thrust added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 100f;
+
+    Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        thrust.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        if(thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
     }
 }
